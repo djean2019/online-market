516fd9790d2515299f18274a65220da236b3f201
Darhyppo <darhyppo@gmail.com>
2020-06-14 15:27:02 -0500

    populate admin and product

---
+++ Back-end/model/product.js
@@ -0,0 +1,48 @@
+/**
+ * Product Model
+ */
+
+const mongoose = require('mongoose')
+
+const product = {
+    'name':{
+        type: String,
+        required: true,
+        validate: {
+            validator: (name) =>{
+                return (name.length >= 4);
+              },
+            message: props => `${props.value} Validation failed`
+        }
+    },
+    'imageUrl': {
+        type: String,
+        required: true
+    },
+    'price': {
+        type: Number,
+        required: true,
+    },
+    'description': {
+        type: String,
+        required: true
+    },
+    'createdDate':{
+        type: Date,
+        default: Date.now
+    }
+    // 'userId': {
+    //     type: String,
+    //     required: true
+    // }
+}
+
+const productSchema = new mongoose.Schema(product);
+const productModel = mongoose.model('product',productSchema);
+
+const productDomain = {
+    'productSchema': productSchema,
+    'productModel': productModel
+}
+
+module.exports = productDomain;
--- Back-end/util/apputil.js
+++ Back-end/util/apputil.js
@@ -4,12 +4,12 @@
  * App Utilities
  */
 const AdminModel = require('../model/admin').adminModel
+const ProductModel = require('../model/product').productModel
 const bcrypt = require('../util/bcrypt')
 
 module.exports = {
     populateAdmin: function (onComplete) {
         let admin = new AdminModel({ fullname: 'Darphe H. J.', email: 'darphe@onlinemarket.com', password: bcrypt.encodeSync('1234567') })
-        // let admin = new AdminModel({ fullname: 'Super Admin', email: 'super@onlinemarket.com', password: '1234567' })
         AdminModel.exists({ email: admin.email })
             .then(res => {
                 if (res) {
@@ -19,5 +19,17 @@ module.exports = {
                     onComplete(`admin created - ${Date.now()}`)
                 }
             }).catch(err => onComplete(`admin failed to create`))
+    },
+    populateProduct: function (onComplete) {
+        let product = new ProductModel({ name: 'Book', price: 30, imageUrl: 'darphe@onlinemarket.com', description: 'description' })
+        ProductModel.exists({ name: product.name })
+            .then(res => {
+                if (res) {
+                    onComplete(`product Already available ${Date.now()}`)
+                } else {
+                    product.save()
+                    onComplete(`product created - ${Date.now()}`)
+                }
+            }).catch(err => onComplete(`product failed to create`))
     }
 }
