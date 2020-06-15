e5688da495d9801575a26846539c394b92a301c6
Darhyppo <darhyppo@gmail.com>
2020-06-15 15:06:54 -0500

    Authentication test

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,45 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-// var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/user-routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// var path = require('path');
 
-var app = express();
+const userRoutes = require('./routes/user-routes');
+const authRoutes = require('./routes/auth-route');
+const auth = require('./util/jwt-auth');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const app = express();
 
-// app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: '*'
-}))
-// app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/admin', usersRouter);
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(cors());
+app.use(bodyParser.json());
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(authRoutes);
+app.use(auth.verifyToken);
+app.use(userRoutes);
 
 module.exports = app;
--- Back-end/controllers/auth-controller.js
+++ Back-end/controllers/auth-controller.js
@@ -6,20 +6,20 @@ const config = require('../config/jwt-properties');
 
 exports.signin = async(req, res, next) => {
     try {
-        const user = await User.findOne({ username: req.body.username });
+        const user = await User.findOne({ email: req.body.email });
         if (user) {
             const isValid = await bcrypt.compare(req.body.password, user.password);
             if (isValid) {
-                const token = jwt.sign({ data: req.body.username }, config.jwtKey, {
+                const token = jwt.sign({ data: req.body.email }, config.jwtKey, {
                     expiresIn: config.jwtExpirySeconds
                 });
                 res.status(200).send(new ResponseApi(200, 'success', { token: token, expiresIn: config.jwtExpirySeconds, user: user }));
             } else {
-                res.status(401).send(new ResponseApi(401, 'error', { err: 'invalid username and/or password' }));
+                res.status(401).send(new ResponseApi(401, 'error', { err: 'invalid email and/or password' }));
             }
 
         } else {
-            res.status(401).send(new ResponseApi(401, 'error', { err: 'invalid username and/or password' }));
+            res.status(401).send(new ResponseApi(401, 'error', { err: 'invalid email and/or password' }));
         }
     } catch (err) {
         res.status(500).send(new ResponseApi(500, 'error', err));
