fea51d47cf03b85312ab18b14f8210f4d0dc3246
Darhyppo <darhyppo@gmail.com>
2020-06-18 13:23:41 -0500

    ...

--- angular-services/controllers/user-controller.js
+++ angular-services/controllers/user-controller.js
@@ -1,5 +1,5 @@
 const User = require('../models/user-model').userModel;
-const Product = require('../models/product-model').productModel;
+// const Product = require('../models/product-model').productModel;
 
 exports.insert = (req, res, next) => {
     User.create(req.body)
