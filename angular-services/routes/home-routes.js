abe98ce63555bf9c3ada306647405ebf6d411867
Carl-Xiao128 <xlx199166@gmail.com>
2020-06-19 02:57:52 -0500

    products list

--- angular-services/routes/home-routes.js
+++ angular-services/routes/home-routes.js
@@ -1,8 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const homeController = require('../controllers/home-controller');
+const homeController = require("../controllers/home-controller");
+const buyerController = require("../controllers/buyer-controller");
 
-router.get('/home', homeController.list);
+router.get("/home", homeController.list);
+router.get("/products", buyerController.list);
 
 module.exports = router;
